92027aedecb6ea0bf4a0f1566676e58d0152eb9d
Yashasvi Dwivedi <email@example.com>
2023-07-11 20:50:16 +0530

    Added placeholders for empty pages, added links to navbar andfooter

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,42 +7,42 @@ export default function Footer()
             <div className="links">
                 <table>
                     <tr>
-                        <td><a href="#">AN OVERVIEW</a></td>
-                        <td><a href="#">ANNUAL REPORTS</a></td>
-                        <td><a href="#">COVER PAGE</a></td>
+                        <td><a href="/An overview">AN OVERVIEW</a></td>
+                        <td><a href="/AnnualReport">ANNUAL REPORTS</a></td>
+                        <td><a href="/">COVER PAGE</a></td>
                     </tr>
                     <tr>
-                        <td><a href="#">DONATE</a></td>
+                        <td><a href="Donate">DONATE</a></td>
                         <td><a href="#">WHAT'S NEXT</a></td>
                         <td><a href="#">ECO-BRICK</a></td>
                     </tr>
                     <tr>
-                        <td><a href="#">FRONT VASTRAAY</a></td>
-                        <td><a href="#">GALLERY</a></td>
-                        <td><a href="#">WHO WE ARE</a></td>
+                        <td><a href="/">FRONT VASTRAAY</a></td>
+                        <td><a href="/Gallery">GALLERY</a></td>
+                        <td><a href="/Who we are">WHO WE ARE</a></td>
                     </tr>
                     <tr>
-                        <td><a href="#">GET INVOLVED</a></td>
+                        <td><a href="/GetInvolved">GET INVOLVED</a></td>
                         <td><a href="#">LOGIN</a></td>
-                        <td><a href="#">OUR FOUNDERS</a></td>
+                        <td><a href="/Our Founders">OUR FOUNDERS</a></td>
                     </tr>
                     <tr>
-                        <td><a href="#">OUR PUBLICATIONS</a></td>
-                        <td><a href="#">OUR STORY</a></td>
+                        <td><a href="/Publications">OUR PUBLICATIONS</a></td>
+                        <td><a href="/Our Story">OUR STORY</a></td>
 
-                        <td><a href="#">OUR TEAM</a></td>
-                    </tr> <tr>   <td><a href="#">PARTNERS</a></td>
+                        <td><a href="/Our Team">OUR TEAM</a></td>
+                    </tr> <tr>   <td><a href="/Partners">PARTNERS</a></td>
                         <td><a href="#">SLIDE ANYTHING POPUP PREVIEW</a></td>
 
 
-                        <td><a href="#">PROJECT S.M.I.L.E</a></td>
+                        <td><a href="/SMILE">PROJECT S.M.I.L.E</a></td>
                     </tr><tr>    <td><a href="#">RESOURCES</a></td>
-                        <td><a href="#">SUPPORT US</a></td>
+                        <td><a href="/Donate">SUPPORT US</a></td>
 
 
-                        <td><a href="#">WATER CONSERVATION</a></td>
-                    </tr>   <tr> <td><a href="#">WET WASTE MANAGEMENT</a></td>
-                        <td><a href="#">WHAT WE DO</a></td>
+                        <td><a href="/Water-conservation">WATER CONSERVATION</a></td>
+                    </tr>   <tr> <td><a href="/Wet-Waste-Management">WET WASTE MANAGEMENT</a></td>
+                        <td><a href="/What We Do">WHAT WE DO</a></td>
 
                     </tr>
                 </table>
