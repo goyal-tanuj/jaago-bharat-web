08080a1f177f2e6e328db122e12fdf2cce5b5018
Yashasvi Dwivedi <DentFuse@users.noreply.github.com>
2023-07-21 15:07:14 +0530

    Fix navbar

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,51 +1,99 @@
-import React from 'react'
-import './App.css'
-export default function Footer()
-{
-    return (
+import React from "react";
+import "./App.css";
+export default function Footer() {
+  return (
+    <div className="links">
+      <table>
+        <tr>
+          <td>
+            <a href="/An overview">AN OVERVIEW</a>
+          </td>
+          <td>
+            <a href="/AnnualReport">ANNUAL REPORTS</a>
+          </td>
+          <td>
+            <a href="/">COVER PAGE</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="Donate">DONATE</a>
+          </td>
+          <td>
+            <a href="#">WHAT'S NEXT</a>
+          </td>
+          <td>
+            <a href="/Eco brick">ECO-BRICK</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="/">FRONT VASTRAAY</a>
+          </td>
+          <td>
+            <a href="/Gallery">GALLERY</a>
+          </td>
+          <td>
+            <a href="/Who we are">WHO WE ARE</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="/GetInvolved">GET INVOLVED</a>
+          </td>
+          <td>
+            <a href="#">LOGIN</a>
+          </td>
+          <td>
+            <a href="/Our Founders">OUR FOUNDERS</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="/Publications">OUR PUBLICATIONS</a>
+          </td>
+          <td>
+            <a href="/Our Story">OUR STORY</a>
+          </td>
 
-            <div className="links">
-                <table>
-                    <tr>
-                        <td><a href="/An overview">AN OVERVIEW</a></td>
-                        <td><a href="/AnnualReport">ANNUAL REPORTS</a></td>
-                        <td><a href="/">COVER PAGE</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href="Donate">DONATE</a></td>
-                        <td><a href="#">WHAT'S NEXT</a></td>
-                        <td><a href="/Eco brick">ECO-BRICK</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href="/">FRONT VASTRAAY</a></td>
-                        <td><a href="/Gallery">GALLERY</a></td>
-                        <td><a href="/Who we are">WHO WE ARE</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href="/GetInvolved">GET INVOLVED</a></td>
-                        <td><a href="#">LOGIN</a></td>
-                        <td><a href="/Our Founders">OUR FOUNDERS</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href="/Publications">OUR PUBLICATIONS</a></td>
-                        <td><a href="/Our Story">OUR STORY</a></td>
-
-                        <td><a href="/Our Team">OUR TEAM</a></td>
-                    </tr> <tr>   <td><a href="/Partners">PARTNERS</a></td>
-                        <td><a href="#">SLIDE ANYTHING POPUP PREVIEW</a></td>
-
-
-                        <td><a href="/SMILE">PROJECT S.M.I.L.E</a></td>
-                    </tr><tr>    <td><a href="#">RESOURCES</a></td>
-                        <td><a href="/Donate">SUPPORT US</a></td>
-
-
-                        <td><a href="/Water-conservation">WATER CONSERVATION</a></td>
-                    </tr>   <tr> <td><a href="/Wet-Waste-Management">WET WASTE MANAGEMENT</a></td>
-                        <td><a href="/What We Do">WHAT WE DO</a></td>
-
-                    </tr>
-                </table>
-            </div>
-    )
+          <td>
+            <a href="/Our Team">OUR TEAM</a>
+          </td>
+        </tr>{" "}
+        <tr>
+          {" "}
+          <td>
+            <a href="/Partners">PARTNERS</a>
+          </td>
+          <td>
+            <a href="#">SLIDE ANYTHING POPUP PREVIEW</a>
+          </td>
+          <td>
+            <a href="/SMILE">PROJECT S.M.I.L.E</a>
+          </td>
+        </tr>
+        <tr>
+          {" "}
+          <td>
+            <a href="#">RESOURCES</a>
+          </td>
+          <td>
+            <a href="/Donate">SUPPORT US</a>
+          </td>
+          <td>
+            <a href="/Water-conservation">WATER CONSERVATION</a>
+          </td>
+        </tr>{" "}
+        <tr>
+          {" "}
+          <td>
+            <a href="/Wet-Waste-Management">WET WASTE MANAGEMENT</a>
+          </td>
+          <td>
+            <a href="/What We Do">WHAT WE DO</a>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,131 +1,213 @@
-import React, { useState , useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import logo from '../images/logo.png'
-import './navbar.css';
-import { IoMdArrowDropdown } from 'react-icons/io';
-import {IoIosArrowDropdown} from 'react-icons/io';
-import { RxHamburgerMenu } from 'react-icons/rx';
-import {RxCross2} from 'react-icons/rx';
-export default function Navbar()
-{
-  const [dropclick, setdropclick] = useState(true)
-  const clickfunction=function(){
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
+import "./navbar.css";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { IoIosArrowDropdown } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
+export default function Navbar() {
+  const [dropclick, setdropclick] = useState(true);
+  const clickfunction = function () {
     setdropclick(!dropclick);
-  }
-  
-  const [dropclick1, setdropclick1] = useState(true)
-  const clickfunction1=function(){
-    setdropclick1(!dropclick1);
-  }
-  
-  const [dropclick2, setdropclick2] = useState(true)
-  const clickfunction2=function(){
-    setdropclick2(!dropclick2);
-  }
-  
-  const [dropclick3, setdropclick3] = useState(true)
-  const clickfunction3=function(){
-    setdropclick3(!dropclick3);
-  }
-  const [showmediaicons, setshowmediaicons] = useState(false)
-  const myFunction = function ()
-  {
-    setshowmediaicons(!showmediaicons);
-  }
-  useEffect(()=>{
-    setdropclick(true)
-    setdropclick1(true)
-    setdropclick2(true)
-    setdropclick3(true)
-  },[showmediaicons]);
-
-  const [changelink, setchangelink] = useState(false)
-  const linkfunction =function(){
-    setchangelink(!changelink);
-  }
-  useEffect(()=>{
-    setshowmediaicons(false)},
-    [changelink]
-  );
-  const [colorChange, setColorchange] = useState(false);
-  
-  const changeNavbarColor = () => {
-      if (window.scrollY >= 80) {
-          setColorchange(false);
-      }
-      else {
-          setColorchange(true);
-      }
   };
-  window.addEventListener('scroll', changeNavbarColor);
- 
+
+  const [dropclick1, setdropclick1] = useState(true);
+  const clickfunction1 = function () {
+    setdropclick1(!dropclick1);
+  };
+
+  const [dropclick2, setdropclick2] = useState(true);
+  const clickfunction2 = function () {
+    setdropclick2(!dropclick2);
+  };
+
+  const [dropclick3, setdropclick3] = useState(true);
+  const clickfunction3 = function () {
+    setdropclick3(!dropclick3);
+  };
+  const [showmediaicons, setshowmediaicons] = useState(false);
+  const myFunction = function () {
+    setshowmediaicons(!showmediaicons);
+  };
+  useEffect(() => {
+    setdropclick(true);
+    setdropclick1(true);
+    setdropclick2(true);
+    setdropclick3(true);
+  }, [showmediaicons]);
+
+  const [changelink, setchangelink] = useState(false);
+  const linkfunction = function () {
+    setchangelink(!changelink);
+  };
+  useEffect(() => {
+    setshowmediaicons(false);
+  }, [changelink]);
+  const [colorChange, setColorchange] = useState(false);
+
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 80) {
+      setColorchange(false);
+    } else {
+      setColorchange(true);
+    }
+  };
+  window.addEventListener("scroll", changeNavbarColor);
+
   return (
     <section className="one">
-      <nav className={colorChange? "mainnav":"mainnav changings"}>
+      <nav className={colorChange ? "mainnav" : "mainnav changings"}>
         <div className="logo">
-          <Link  onClick={linkfunction} to="/"><img src={logo} alt="" /></Link>
+          <Link onClick={linkfunction} to="/">
+            <img src={logo} alt="" />
+          </Link>
         </div>
-        <div className={showmediaicons ? "menu-link mobile-menu-link" : "menu-link"}>
+        <div
+          className={
+            showmediaicons ? "menu-link mobile-menu-link" : "menu-link"
+          }
+        >
           <ul>
-            <li className={dropclick ?"dropclass":"drop"}>
-              <Link  onClick={linkfunction} to="/Who we are" >Who we are </Link><span className="icondrop" style={{color:'white'}}>  <IoMdArrowDropdown /></span>
-               <button onClick={clickfunction}>
-                {dropclick ?<IoIosArrowDropdown/>:"x"}</button>
+            <li className={dropclick ? "dropclass" : "drop"}>
+              <Link onClick={linkfunction} to="/Who we are">
+                Who we are{" "}
+              </Link>
+              <span className="icondrop" style={{ color: "white" }}>
+                {" "}
+                <IoMdArrowDropdown />
+              </span>
+              <button onClick={clickfunction}>
+                {dropclick ? <IoIosArrowDropdown /> : "x"}
+              </button>
               <div className="dropdown">
                 <ul>
-                  <li><Link  onClick={linkfunction} to='/Our story'>Our story</Link></li>
-                  <li><Link  onClick={linkfunction} to='/Our Founders'>Our founder</Link></li>
-                  <li><Link  onClick={linkfunction} to='/Our team'>Our team</Link></li>
+                  <li>
+                    <Link onClick={linkfunction} to="/Our story">
+                      Our story
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={linkfunction} to="/Our Founders">
+                      Our founder
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={linkfunction} to="/Our team">
+                      Our team
+                    </Link>
+                  </li>
                 </ul>
               </div>
             </li>
-            <li className={dropclick1 ?"dropclass":"drop1"}>
-              <Link  onClick={linkfunction} to="/What we do" >What we do </Link><span className="icondrop" style={{color:'white'}}><IoMdArrowDropdown /></span>
-                <button onClick={clickfunction1}>{ dropclick1?<IoIosArrowDropdown/>:"x"}</button>
+            <li className={dropclick1 ? "dropclass" : "drop1"}>
+              <Link onClick={linkfunction} to="/What we do">
+                What we do{" "}
+              </Link>
+              <span className="icondrop" style={{ color: "white" }}>
+                <IoMdArrowDropdown />
+              </span>
+              <button onClick={clickfunction1}>
+                {dropclick1 ? <IoIosArrowDropdown /> : "x"}
+              </button>
               <div className="dropdown">
                 <ul>
-                  <li><Link  onClick={linkfunction} to='/An overview'>An overview</Link></li>
-                  <li><Link  onClick={linkfunction} to='/Eco brick'>Eco-brick</Link></li>
-                  <li><Link  onClick={linkfunction} to='/Wet-Waste-Management'>Wet waste management</Link></li>
-                  <li><Link  onClick={linkfunction} to='/Water-conservation'>water conservation</Link></li>
-                  <li><Link  onClick={linkfunction} to='/SMILE'>project s.m.i.l.e</Link></li>
+                  <li>
+                    <Link onClick={linkfunction} to="/An overview">
+                      An overview
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={linkfunction} to="/Eco brick">
+                      Eco-brick
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={linkfunction} to="/Wet-Waste-Management">
+                      Wet waste management
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={linkfunction} to="/Water-conservation">
+                      water conservation
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={linkfunction} to="/SMILE">
+                      project s.m.i.l.e
+                    </Link>
+                  </li>
                 </ul>
               </div>
             </li>
             <li>
-              <Link  onClick={linkfunction} to="/Partners" >Partners</Link>
+              <Link onClick={linkfunction} to="/Partners">
+                Partners
+              </Link>
             </li>
-             <li className={dropclick2 ?"dropclass":"drop2"}>
-              <Link  onClick={linkfunction} to="#" >Resources </Link><span className="icondrop" style={{color:'white'}}><IoMdArrowDropdown /></span><
-                button onClick={clickfunction2}>{dropclick2?<IoIosArrowDropdown/>:"x"}</button>
+            <li className={dropclick2 ? "dropclass" : "drop2"}>
+              <Link onClick={linkfunction} to="#">
+                Resources{" "}
+              </Link>
+              <span className="icondrop" style={{ color: "white" }}>
+                <IoMdArrowDropdown />
+              </span>
+              <button onClick={clickfunction2}>
+                {dropclick2 ? <IoIosArrowDropdown /> : "x"}
+              </button>
               <div className="dropdown">
                 <ul>
-                  <li><Link  onClick={linkfunction} to='/AnnualReport'>Annual report</Link></li>
-                  <li><Link  onClick={linkfunction} to='/Publications'>Our publications</Link></li>
-                  <li><Link  onClick={linkfunction} to='/Gallery'>Our gallery</Link></li>
+                  <li>
+                    <Link onClick={linkfunction} to="/AnnualReport">
+                      Annual report
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={linkfunction} to="/Publications">
+                      Our publications
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={linkfunction} to="/Gallery">
+                      Our gallery
+                    </Link>
+                  </li>
                 </ul>
               </div>
             </li>
             <li>
-              <Link  onClick={linkfunction} to="/GetInvolved" >Get Involved</Link>
+              <Link onClick={linkfunction} to="/GetInvolved">
+                Get Involved
+              </Link>
             </li>
-            <li className={dropclick3 ?"dropclass":"drop3"}>
-              <Link  onClick={linkfunction} to="#" >Support us </Link><span className="icondrop" style={{color:'white'}}><IoMdArrowDropdown /></span>
-              <button onClick={clickfunction3}>{dropclick3?<IoIosArrowDropdown/>:"x"}</button>
+            <li className={dropclick3 ? "dropclass" : "drop3"}>
+              <Link onClick={linkfunction} to="#">
+                Support us{" "}
+              </Link>
+              <span className="icondrop" style={{ color: "white" }}>
+                <IoMdArrowDropdown />
+              </span>
+              <button onClick={clickfunction3}>
+                {dropclick3 ? <IoIosArrowDropdown /> : "x"}
+              </button>
               <div className="dropdown">
                 <ul>
-                  <li><Link  onClick={linkfunction} to='/Donate'>Donate</Link></li>
+                  <li>
+                    <Link onClick={linkfunction} to="/Donate">
+                      Donate
+                    </Link>
+                  </li>
                 </ul>
               </div>
             </li>
           </ul>
           <div className="icon">
-            <Link to="#" onClick={myFunction}>{showmediaicons ? <RxCross2/> : <RxHamburgerMenu/>}</Link>
-            <Link  onClick={linkfunction} to="#" href="javascript:void(0);"
-              onClick={myFunction}>{showmediaicons ? <RxCross2/> : <RxHamburgerMenu/>}</Link>
+            <Link to="#" onClick={myFunction}>
+              {showmediaicons ? <RxCross2 /> : <RxHamburgerMenu />}
+            </Link>
           </div>
         </div>
       </nav>
     </section>
-  )
+  );
 }
