78bc5057702b11498b2178ec8b5164d494cf1a06
Yashasvi Dwivedi <DentFuse@users.noreply.github.com>
2023-12-30 22:58:17 +0530

    added sessionStorage caching for blogs

--- src/components/loader.js
+++ src/components/loader.js
@@ -3,9 +3,7 @@ import removeMD from "markdown-to-text";
 const url = "https://readblog-w4dwmgxs2a-uc.a.run.app?id=";
 
 export default async function Main(id, setState) {
-  const response = await fetch(url + id);
-  const post = await response.json();
-  console.log(post);
+  const post = await getPost(id);
   post.desc = post.desc.replaceAll("\\n", "\n");
   post.plain = removeMD(post.desc);
   setState(post);
@@ -15,8 +13,7 @@ export async function homeList(num, setList) {
   const out = [];
   const list = [2, 4, 5];
   for (let i = 0; i < num; i++) {
-    const response = await fetch(url + list[i]);
-    const post = await response.json();
+    const post = await getPost(i);
     post.link = "/blog/" + i;
     post.desc = post.desc.replaceAll("\\n", "\n");
     post.plain = removeMD(post.desc);
@@ -28,12 +25,27 @@ export async function homeList(num, setList) {
 export async function listData(num, setList) {
   const out = [];
   for (let i = 0; i < num; i++) {
-    const response = await fetch(url + i);
-    const post = await response.json();
+    const post = await getPost(i);
     post.link = "/blog/" + i;
     post.desc = post.desc.replaceAll("\\n", "\n");
     post.plain = removeMD(post.desc);
     out.push(post);
   }
   setList(out);
+}
+
+function getPost(id) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (sessionStorage.getItem(id)) {
+        return resolve(JSON.parse(sessionStorage.getItem(id)));
+      }
+      const response = await fetch(url + id);
+      const post = await response.json();
+      sessionStorage.setItem(id, JSON.stringify(post));
+      return resolve(post);
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
